7f2aff61ec4f01fa0fa5462a36a0b0de9d191517
Master <MasterPiyasirisilp@Masters-MacBook-Pro.local>
2016-01-23 19:08:16 -0500

    Fix it

---
+++ Documents/Android Studio/EventRegistration/app/src/main/java/ca/mcgill/ecse321/eventregistration/MainActivity.java
@@ -0,0 +1,173 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationController;
+import ca.mcgill.ecse321.eventregistration.controller.InvalidInputException;
+import ca.mcgill.ecse321.eventregistration.model.Participant;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private HashMap<Integer, Participant> participants;
+    private RegistrationManager rm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        refreshData();
+    }
+
+    private void refreshData(){
+        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
+        tv.setText("");
+
+        //initialize the data in the participant spinner
+        Spinner spinner = (Spinner) findViewById(R.id.participantspinner);
+        ArrayAdapter<CharSequence> participantAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+        participantAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.participants = new HashMap<Integer, Participant>();
+
+        int i = 0;
+        for (Iterator<Participant> participants = rm.getParticipants().iterator(); participants.hasNext(); i++){
+            Participant p = participants.next();
+            participantAdapter.add(p.getName());
+            this.participants.put(i, p);
+        }
+
+        spinner.setAdapter(participantAdapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addParticipant(View v){
+        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
+        EventRegistrationController pc = new EventRegistrationController();
+        try{
+            pc.createParticipant(tv.getText().toString());
+        }   catch (InvalidInputException e){
+            //TODO handle error
+        }
+
+        refreshData();
+    }
+
+    public void showDatePickerDialog(View v){
+        TextView tf = (TextView) v;
+        Bundle args = getDateFromLabel(tf.getText());
+        args.putInt("id", v.getId());
+
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.setArguments(args);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    private Bundle getTimeFromLabel(CharSequence text){
+        Bundle rtn = new Bundle();
+        String comps[] = text.toString().split(":");
+        int hour = 12;
+        int minute = 0;
+
+        if (comps.length == 2){
+            hour = Integer.parseInt(comps[0]);
+            minute = Integer.parseInt(comps[1]);
+        }
+
+        rtn.putInt("hour", hour);
+        rtn.putInt("minute", minute);
+
+        return rtn;
+    }
+
+    private Bundle getDateFromLabel(CharSequence text){
+        Bundle rtn = new Bundle();
+        String comps[] = text.toString().split("-");
+        int day = 1;
+        int month = 1;
+        int year = 1;
+
+        if (comps.length == 3){
+            day = Integer.parseInt(comps[0]);
+            month = Integer.parseInt(comps[1]);
+            year = Integer.parseInt(comps[2]);
+        }
+
+        rtn.putInt("day", day);
+        rtn.putInt("month", month-1);
+        rtn.putInt("year", year);
+
+        return rtn;
+    }
+
+    public void setTime(int id, int h, int m){
+        TextView tv = (TextView) findViewById(id);
+        tv.setText(String.format("%02d:%02d", h, m));
+    }
+
+    public void setDate(int id, int d, int m, int y){
+        TextView tv = (TextView) findViewById(id);
+        tv.setText(String.format("%02d-%02d-%04d", d, m+1, y));
+    }
+
+
+
+    public void addEvent(View v){
+        TextView tv = (TextView) findViewById(R.id.newevent_name);
+        EventRegistrationController pc = new EventRegistrationController();
+        try{
+            pc.createEvent(tv.getText().toString(), );
+        }   catch (InvalidInputException e){
+            //TODO handle error
+        }
+    }
+
+
+}
